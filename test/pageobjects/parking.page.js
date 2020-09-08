8bf5d6ae35df146a2c9fb60a34655b8e90bd5713
yoshuadupin <djosue@unitec.edu>
2020-09-08 17:01:46 -0600

    feat: ParkingPage function for estimated and radioButtom

--- test/pageobjects/parking.page.js
+++ test/pageobjects/parking.page.js
@@ -2,13 +2,23 @@ const Page = require('./page');
 
 class ParkingPage extends Page {
 
-    get comboBoxParkingLot() { return $('[id="ParkingLot"]') }
-    get inputStartingDate() { return  $('[id="StartingDate"]')}
-    get inputLeavingDate() { return  $('[id="LeavingDate"]')}
-    get inputStartingTime() { return  $('[id="StartingTime"]')}
-    get inputLeavingTime() { return  $('[id="LeavingTime"]')}
-    get radioButoomStartingAM() { return  $('[id="StartingTime"]')}
-    get radioButtomStartingPM() { return  $('[id="LeavingTime"]')}
+    get comboBoxParkingLot() { return $('[name="ParkingLot"]') }
+
+    get inputStartingDate() { return  $('[name="StartingDate"]')}
+    get inputLeavingDate() { return  $('[name="LeavingDate"]')}
+    
+    get inputStartingTime() { return  $('[name="StartingTime"]')}
+    get inputLeavingTime() { return  $('[name="LeavingTime"]')}
+    
+    get radioButoomStartingAM() { return  $('[name="StartingTimeAMPM"]').$('[value="AM"]')}
+    get radioButtomStartingPM() { return  $('[name="StartingTimeAMPM"]').$('[value="PM"]')}
+    
+    get radioButoomLeavingAM() { return  $('[name="LeavingTimeAMPM"]').$('[value="AM"]')}
+    get radioButtomLeavingPM() { return  $('[name="LeavingTimeAMPM"]').$('[value="PM"]')}
+
+    get estimatedParkingCost(){return $('//body/form/table/tbody/tr[4]/td[2]/span[1]/b')}
+    get estimatedParkingTime(){return $('//body/form/table/tbody/tr[4]/td[2]/span[2]/b')}
+
     
 
     open() {
