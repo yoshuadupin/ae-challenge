ed4e25916ebcf9fffae1d427205a0e4a61a17138
yoshuadupin <djosue@unitec.edu>
2020-09-08 17:40:13 -0600

    No major changes

--- test/pageobjects/parking.page.js
+++ test/pageobjects/parking.page.js
@@ -1,26 +1,27 @@
 
-module.exports =class ParkingPage   {
-
-    get comboBoxParkingLot() { return $('[name="ParkingLot"]') }
-
-    get inputStartingDate() { return  $('[name="StartingDate"]')}
-    get inputLeavingDate() { return  $('[name="LeavingDate"]')}
-    
-    get inputStartingTime() { return  $('[name="StartingTime"]')}
-    get inputLeavingTime() { return  $('[name="LeavingTime"]')}
-    
-    get radioButtonStartingAM() { return  $('[name="StartingTimeAMPM"]').$('[value="AM"]')}
-    get radioButtonStartingPM() { return  $('[name="StartingTimeAMPM"]').$('[value="PM"]')}
-    
-    get radiButtonLeavingAM() { return  $('[name="LeavingTimeAMPM"]').$('[value="AM"]')}
-    get radioButtonLeavingPM() { return  $('[name="LeavingTimeAMPM"]').$('[value="PM"]')}
-
-    get estimatedParkingCost(){return $('//body/form/table/tbody/tr[4]/td[2]/span[1]/b')}
-    get estimatedParkingTime(){return $('//body/form/table/tbody/tr[4]/td[2]/span[2]/b')}
-
-    get buttonCalculate(){return $('input[name=Calculte]')}
-
+module.exports = class ParkingPage {
     open() {
         return browser.url(`http://www.shino.de/parkcalc/`)
     }
+
+    get comboBoxParkingLot() { return $('[name="ParkingLot"]') }
+
+    get inputStartingDate() { return $('[name="StartingDate"]') }
+    get inputLeavingDate() { return $('[name="LeavingDate"]') }
+
+    get inputStartingTime() { return $('[name="StartingTime"]') }
+    get inputLeavingTime() { return $('[name="LeavingTime"]') }
+
+    get radioButtonStartingAM() { return $('//body/form/table/tbody/tr[2]/td[2]/input[3]') }
+    get radioButtonStartingPM() { return $('///body/form/table/tbody/tr[2]/td[2]/input[4]') }
+
+    get radiButtonLeavingAM() { return $('//body/form/table/tbody/tr[3]/td[2]/input[3]') }
+    get radioButtonLeavingPM() { return $('//body/form/table/tbody/tr[3]/td[2]/input[4]') }
+
+    get estimatedParkingCost() { return $('//body/form/table/tbody/tr[4]/td[2]/span[1]/b') }
+    get estimatedParkingTime() { return $('//body/form/table/tbody/tr[4]/td[2]/span[2]/b') }
+
+    get buttonCalculate() { return $('input[name=Calculte]') }
+
+   
 }
---
+++ test/specs/basic.spec.js
@@ -0,0 +1,7 @@
+const ParkingPage = require('../pageobjects/parking.page');
+
+describe('Basic elements interact' , ()=> {
+    it ('should allow interact with the elements',()=>{
+        ParkingPage.open();
+    })
+})
