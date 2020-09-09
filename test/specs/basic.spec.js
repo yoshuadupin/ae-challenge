05401cff17e57e44375f297546051246bdff8538
yoshuadupin <djosue@unitec.edu>
2020-09-09 12:38:25 -0600

    feat: simple test for elements

--- test/specs/basic.spec.js
+++ test/specs/basic.spec.js
@@ -1,4 +1,11 @@
 const ParkingPage = require('../pageobjects/parking.page');
+const parkingLot = {
+    VALETPARKING: 1
+    , SHORTTERM: 2
+    , ECONOMY: 3
+    , LONGTEMGARAGE: 4
+    , LONGTEMSURFACE: 5
+}
 
 
 describe('Basic elements interact', () => {
@@ -7,9 +14,21 @@ describe('Basic elements interact', () => {
         //browser.pause(3000);
     })
 
-    it('should allow interact with the elements', () => {
-        const optionParkingRate = ParkingPage.comboBoxParkingLot().$(`//option[${4}]`).getText();
-        console.log(optionParkingRate)
-
+    it('should allow make a simple calculate', () => {
+        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.inputStartingDate().setValue('9/9/2020');
+        ParkingPage.inputStartingTime().setValue('10:00');
+        ParkingPage.radioButtonStartingPM().click()
+        browser.pause(1000);
+        ParkingPage.radioButtonStartingAM().click();
+        browser.pause(1000);
+        ParkingPage.inputLeavingDate().setValue('9/9/2020');
+        ParkingPage.inputLeavingTime().setValue('11:00');
+        ParkingPage.radioButtonLeavingPM().click()
+        browser.pause(1000);
+        ParkingPage.radioButtonLeavingAM().click();
+        browser.pause(1000);
+        ParkingPage.buttonCalculate().click();
+        browser.pause(4000);
     });
 });
