c06fc45ca09f6cb6157593c7f94ed5f0806e8417
yoshuadupin <djosue@unitec.edu>
2020-09-08 18:25:11 -0600

    finish testing methods of ParkingPage

--- test/specs/basic.spec.js
+++ test/specs/basic.spec.js
@@ -5,19 +5,27 @@ const ParkingPage = require('../pageobjects/parking.page');
 describe('Basic elements interact', () => {
     it('should allow interact with the elements', () => {
         ParkingPage.open();
-        console.log(ParkingPage.buttonCalculate().getTagName());
-        console.log(ParkingPage.comboBoxParkingLot().getTagName());
-        console.log(ParkingPage.estimatedParkingCost().getTagName());
-        console.log(ParkingPage.estimatedParkingTime().getTagName());
-        console.log(ParkingPage.inputLeavingDate().getTagName());
-        console.log(ParkingPage.inputLeavingTime().getTagName());
-        console.log(ParkingPage.inputStartingDate().getTagName());
-        console.log(ParkingPage.inputStartingTime().getTagName());
-        console.log(ParkingPage.radioButtonLeavingAM().getTagName());
-        console.log(ParkingPage.radioButtonLeavingPM().getTagName());
-        console.log(ParkingPage.radioButtonStartingAM().getTagName());
-        console.log(ParkingPage.radioButtonStartingPM().getTagName());
+        let array  =[];
+        array.push(ParkingPage.buttonCalculate().getTagName());
+        array.push(ParkingPage.comboBoxParkingLot().getTagName());
+       
+        array.push(ParkingPage.inputLeavingDate().getTagName());
+        array.push(ParkingPage.inputLeavingTime().getTagName());
+        array.push(ParkingPage.inputStartingDate().getTagName());
+        array.push(ParkingPage.inputStartingTime().getTagName());
+        array.push(ParkingPage.radioButtonLeavingAM().getTagName());
+        array.push(ParkingPage.radioButtonLeavingPM().getTagName());
+        array.push(ParkingPage.radioButtonStartingAM().getTagName());
+        array.push(ParkingPage.radioButtonStartingPM().getTagName());
+        
+        const exitCost =  ParkingPage.estimatedParkingCost().isExisting();
+        const exitTime =  ParkingPage.estimatedParkingTime().isExisting();
 
+        exitCost ? array.push(ParkingPage.estimatedParkingCost().getTagName()):console.log("No existe");
+        exitTime ? array.push(ParkingPage.estimatedParkingTime().getTagName()):console.log("No existe");
+
+        
+        console.log(array)
 
     });
 });
