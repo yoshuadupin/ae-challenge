57565bbea5419a15f08e9cae7ac122910399a939
yoshuadupin <djosue@unitec.edu>
2020-09-08 19:29:25 -0600

    optionParkingRate

--- test/specs/basic.spec.js
+++ test/specs/basic.spec.js
@@ -4,11 +4,11 @@ const ParkingPage = require('../pageobjects/parking.page');
 describe('Basic elements interact', () => {
     before(() => {
         ParkingPage.open();
-        browser.pause(3000);
+        //browser.pause(3000);
     })
 
     it('should allow interact with the elements', () => {
-        const optionParkingRate = ParkingPage.comboBoxParkingLot().$('option').getText();
+        const optionParkingRate = ParkingPage.comboBoxParkingLot().$(`//option[${4}]`).getText();
         console.log(optionParkingRate)
 
     });
