e3ac2a7ed68f642ab7aaad2d2f5dbc3cbd15062c
yoshuadupin <djosue@unitec.edu>
2020-09-09 14:10:01 -0600

    feat: test should allow less five hours Valet Parking finish

--- test/specs/valetParking.spec.js
+++ test/specs/valetParking.spec.js
@@ -1,4 +1,5 @@
 const ParkingPage = require('../pageobjects/parking.page');
+const { radioButtonLeavingPM } = require('../pageobjects/parking.page');
 
 
 const parkingLot = {
@@ -14,25 +15,55 @@ describe('Valet Parking Test', () => {
         ParkingPage.open();
     })
     // DONE
+    //TODO: test de debe mantenerse en el mismo estado  
+    it('should allow make less five hour  Valet Parking', () => {
+        const inputStartHour = 10;
+        const expectCost = "$ 12.00";
 
-    it('should allow make different rate Valet Parking', () => {
-        const startHour = 1;
         ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
         ParkingPage.inputStartingDate().setValue('9/9/2020');
         ParkingPage.inputLeavingDate().setValue('9/9/2020');
 
+        for (let i = 1; i <= 5; i++) {
+            ParkingPage.inputStartingTime().setValue(`${inputStartHour}:00`);
+            ParkingPage.inputLeavingTime().setValue(`${(inputStartHour + i) % 12}:00`);
+
+            ParkingPage.radioButtonLeavingAM().click();
+
+            if (inputStartHour + i >= 12) {
+                ParkingPage.radioButtonLeavingPM().click();
+            }
+
+            ParkingPage.buttonCalculate().click();
+            expect(ParkingPage.estimatedParkingCost()).toBeDisplayed();
+            expect(ParkingPage.estimatedParkingTime()).toBeDisplayed();
+            const actualCost = ParkingPage.estimatedParkingCost().getText();
+            expect(actualCost).toEqual(expectCost);
+        }
 
 
-        ParkingPage.inputStartingTime().setValue(`${startHour}:00`);
-        ParkingPage.inputLeavingTime().setValue(`${startHour+5}:00`);
+        for (let i = 1; i <= 5; i++) {
+            ParkingPage.inputStartingTime().setValue(`${inputStartHour}:00`);
+            ParkingPage.inputLeavingTime().setValue(`${(inputStartHour + i) % 12}:00`);
+
+            ParkingPage.radioButtonLeavingPM().click();
+            ParkingPage.radioButtonStartingPM().click();
 
 
-        ParkingPage.buttonCalculate().click();
-        browser.pause(3000);
-        expect(ParkingPage.estimatedParkingCost()).toBeDisplayed();
-        expect(ParkingPage.estimatedParkingTime()).toBeDisplayed();
+            if (inputStartHour + i >= 12) {
+                ParkingPage.radioButtonLeavingAM().click();
+                ParkingPage.inputLeavingDate().setValue('9/10/2020');
+            }
+
+            ParkingPage.buttonCalculate().click();
+            expect(ParkingPage.estimatedParkingCost()).toBeDisplayed();
+            expect(ParkingPage.estimatedParkingTime()).toBeDisplayed();
+            const actualCost = ParkingPage.estimatedParkingCost().getText();
+            expect(actualCost).toEqual(expectCost);
+        }
+
         browser.deleteCookies();
-        browser.pause(2000);
     });
+
 
 });
