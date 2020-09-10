3815123a856b7a9b7fc67f18e4700aea8ee44bf9
yoshuadupin <djosue@unitec.edu>
2020-09-09 20:44:51 -0600

    feat: test valet parking per day finish

--- test/specs/valetParking.spec.js
+++ test/specs/valetParking.spec.js
@@ -1,5 +1,5 @@
 const ParkingPage = require('../pageobjects/parking.page');
-const chai =  require('chai')
+const chai = require('chai')
 const parkingLot = {
     VALETPARKING: 1
     , SHORTTERM: 2
@@ -62,86 +62,58 @@ describe('valet parking Tests', () => {
         }
 
     });
-    xit('should calculate cost of more five hour Valet Parking', () => {
-        const inputStartHour = 10;
-        const expectCost = "$ 12.00";
+
+    it('should calculate cost per day valet parking', () => {
+        const inputStartDay = 9;
 
         ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
-        ParkingPage.inputStartingDate().setValue('9/9/2020');
-        ParkingPage.inputLeavingDate().setValue('9/9/2020');
+        ParkingPage.inputStartingDate().setValue(`9/${inputStartDay}/2020`);
+        ParkingPage.inputLeavingDate().setValue(`9/${inputStartDay}/2020`);
+        ParkingPage.inputStartingTime().setValue(`1:00`);
+        ParkingPage.inputLeavingTime().setValue(`7:00`);
+        ParkingPage.buttonCalculate().click();
+ 
+        const actualCost = ParkingPage.estimatedParkingCost();
+
+        expect(actualCost.getText()).toEqual(`$ 18.00`);
+        ParkingPage.inputLeavingTime().setValue(`1:00`);
 
         for (let i = 1; i <= 5; i++) {
-            ParkingPage.inputStartingTime().setValue(`${inputStartHour}:00`);
-            ParkingPage.inputLeavingTime().setValue(`${(inputStartHour + i) % 12}:00`);
-
-            ParkingPage.radioButtonLeavingAM().click();
-
-            if (inputStartHour + i >= 12) {
-                ParkingPage.radioButtonLeavingPM().click();
-            }
-
-            ParkingPage.buttonCalculate().click();
-            expect(ParkingPage.estimatedParkingCost()).toBeDisplayed();
-            expect(ParkingPage.estimatedParkingTime()).toBeDisplayed();
-            const actualCost = ParkingPage.estimatedParkingCost().getText();
-
-            expect(actualCost).toEqual(expectCost);
+            const expectCost = 18 * i;
+            
+            ParkingPage.inputLeavingDate().setValue(`9/${inputStartDay + i}/2020`);
+            ParkingPage.buttonCalculate().click();   
+            expect(actualCost.getText()).toEqual(`$ ${expectCost}.00`);
         }
-
-
-        for (let i = 1; i <= 5; i++) {
-            ParkingPage.inputStartingTime().setValue(`${inputStartHour}:00`);
-            ParkingPage.inputLeavingTime().setValue(`${(inputStartHour + i) % 12}:00`);
-
-            ParkingPage.radioButtonLeavingPM().click();
-            ParkingPage.radioButtonStartingPM().click();
-
-
-            if (inputStartHour + i >= 12) {
-                ParkingPage.radioButtonLeavingAM().click();
-                ParkingPage.inputLeavingDate().setValue('9/10/2020');
-            }
-
-            ParkingPage.buttonCalculate().click();
-            expect(ParkingPage.estimatedParkingCost()).toBeDisplayed();
-            expect(ParkingPage.estimatedParkingTime()).toBeDisplayed();
-            const actualCost = ParkingPage.estimatedParkingCost().getText();
-            expect(actualCost).toEqual(expectCost);
-        }
-
     });
 
     xit('should calculate time of  valet parking', () => {
         const expectDateStart = new Date(2020, 9, 10, 6, 30, 0);
         const expectDateLeave = new Date(2020, 9, 10, 10, 0, 0);
-       
+
         ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
         ParkingPage.inputStartingDate().setValue('10/9/2020');
         ParkingPage.inputLeavingDate().setValue('10/9/2020');
-
         ParkingPage.radioButtonLeavingAM().click();
         ParkingPage.radioButtonStartingAM().click();
-
         ParkingPage.inputStartingTime().setValue(`6:30`);
         ParkingPage.inputLeavingTime().setValue(`10:00`);
-
         ParkingPage.buttonCalculate().click();
-        
-        const actualTime = ParkingPage.estimatedParkingTime().getText().match(/\d+/g);
 
+        const actualTime = ParkingPage.estimatedParkingTime().getText().match(/\d+/g);
         const actualDay = actualTime[0];
         const actualHour = actualTime[1];
         const actualMin = actualTime[2];
-        
-        
+
         const expectTimeInMinutes = (expectDateLeave - expectDateStart) / (1000 * 60);
-        const expectDay =  Math.floor(expectTimeInMinutes / (60 * 24));
+        const expectDay = Math.floor(expectTimeInMinutes / (60 * 24));
         const expectHour = Math.floor(expectTimeInMinutes / 60);
-        const expectMin  = expectTimeInMinutes % 60;
+        const expectMin = expectTimeInMinutes % 60;
 
         expect(actualDay).toEqual(expectDay.toString());
         expect(actualHour).toEqual(expectHour.toString());
         expect(actualMin).toEqual(expectMin.toString());
+
     });
 
 
