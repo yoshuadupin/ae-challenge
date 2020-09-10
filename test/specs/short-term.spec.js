379df18fd84797fbd17558efffdd265e9d61b3fb
yoshuadupin <djosue@unitec.edu>
2020-09-10 10:57:14 -0600

    fixing error in should calculate test

--- test/specs/short-term.spec.js
+++ test/specs/short-term.spec.js
@@ -1,5 +1,5 @@
 const ParkingPage = require('../pageobjects/parking.page');
-const shortTerm = require ('../utilities').SHORTTERM;
+const shortTerm = require('../utilities').SHORTTERM;
 describe('Short-Term Functional Tests', () => {
     before(() => {
         ParkingPage.open();
@@ -7,9 +7,10 @@ describe('Short-Term Functional Tests', () => {
 
     it('should calculate per hour parking', () => {
         const startHour = 0;
-        const clickShortTerm = () =>ParkingPage.comboBoxParkingLot().$(`//option[${shortTerm}]`).click();
+        const actualCost = ParkingPage.estimatedParkingCost();
+        const parkingLot = ParkingPage.comboBoxParkingLot().$(`//option[${shortTerm}]`);
 
-        clickShortTerm();
+        parkingLot.click();
         ParkingPage.inputStartingDate().setValue("9/9/2020");
         ParkingPage.inputLeavingDate().setValue("9/9/2020");
         ParkingPage.radioButtonLeavingAM().click();
@@ -17,12 +18,13 @@ describe('Short-Term Functional Tests', () => {
         ParkingPage.inputStartingTime().setValue(`${startHour}:00`);
         ParkingPage.inputLeavingTime().setValue(`${startHour + 1}:00`);
         ParkingPage.buttonCalculate().click();
-        const actualCost = ParkingPage.estimatedParkingCost().getText();
-        expect(actualCost).toEqual("$ 2.00");
+        expect(actualCost.getText()).toEqual("$ 2.00");
         //Ciclo para calcular una hora
+        let acumulateCost = 0;
         for (let i = 1; i < 24; i += 0.5) {
             const hours = Math.floor(startHour + i) % 12;
-            const mins = (60 * i) % 60
+            const mins = (60 * i) % 60;
+            parkingLot.click();
 
             if (i < 12) {
                 ParkingPage.inputLeavingTime().setValue(`${hours}:${mins}`);
@@ -30,9 +32,12 @@ describe('Short-Term Functional Tests', () => {
                 ParkingPage.radioButtonLeavingPM().click();
                 ParkingPage.inputLeavingTime().setValue(`${hours}:${mins}`);
             }
+
             ParkingPage.buttonCalculate().click();
             const actualCost = ParkingPage.estimatedParkingCost().getText();
-            expect(actualCost).toEqual(`$ ${2+i*2}.00`);
-        }
+           
+            expect(actualCost).toEqual(`$ ${2 + acumulateCost}.00`);
+            acumulateCost += 1;
+         }
     })
 })
