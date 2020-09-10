8623fcac8409f1b2db2725e8a0c3abff9be83f0e
yoshuadupin <djosue@unitec.edu>
2020-09-10 12:58:35 -0600

    Bug find it for what is daily maximun

--- test/specs/long-term-garage.spec.js
+++ test/specs/long-term-garage.spec.js
@@ -1,40 +1,31 @@
 const ParkingPage = require('../pageobjects/parking.page');
-const longTermSurface = require('../utilities').LONGTEMGARAGE;
+const longTermGarage = require('../utilities').LONGTEMGARAGE;
+
+
 describe('Long-Term Functional Tests' , ()=>{
+    before(() => {
+        ParkingPage.open();
+    })
+
     it('should calculate per hour parking ' , ()=>{
         const startHour = 0;
         const actualCost = ParkingPage.estimatedParkingCost();
-        const parkingLot = ParkingPage.comboBoxParkingLot().$(`//option[${longTermSurface}]`);
+        const parkingLot = ParkingPage.comboBoxParkingLot().$(`//option[${longTermGarage}]`);
 
-        parkingLot.click();
         ParkingPage.inputStartingDate().setValue("9/9/2020");
         ParkingPage.inputLeavingDate().setValue("9/9/2020");
-        ParkingPage.radioButtonLeavingAM().click();
         ParkingPage.radioButtonStartingAM().click();
         ParkingPage.inputStartingTime().setValue(`${startHour}:00`);
-        ParkingPage.inputLeavingTime().setValue(`${startHour + 1}:00`);
-        ParkingPage.buttonCalculate().click();
-        expect(actualCost.getText()).toEqual("$ 2.00");
         //Ciclo para calcular una hora
         //Revisa solo hasta 12 horas por que ese el
         // daily maximun que se maneja en la pagina pero no lo dice
-        let acumulateCost = 0;
-        for (let i = 1; i < 12; i += 0.5) {
-            const hours = Math.floor(startHour + i) % 12;
-            const mins = (60 * i) % 60;
-
+        for (let i = 1; i < 12; i ++) {
             parkingLot.click();
-
-            if (i < 12) {
-                ParkingPage.inputLeavingTime().setValue(`${hours}:${mins}`);
-            } else {
-                ParkingPage.radioButtonLeavingPM().click();
-                ParkingPage.inputLeavingTime().setValue(`${hours}:${mins}`);
-            }
-
+            ParkingPage.inputLeavingTime().setValue(`${startHour+i}:00`);
+            ParkingPage.radioButtonLeavingAM().click();
             ParkingPage.buttonCalculate().click();
-            expect(actualCost.getText()).toEqual(`$ ${2 + acumulateCost}.00`);
-            acumulateCost += 1;
+            expect(actualCost.getText()).toEqual(`$ ${2 * i}.00`);
+            browser.pause(2000);
         }
     });
 })
