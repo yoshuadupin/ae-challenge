543061b6127d6035b5c8bbb9414ab9fcb2e4c51c
yoshuadupin <djosue@unitec.edu>
2020-09-10 21:30:51 -0600

    Testing sauce

--- test/specs/failure.spec.js
+++ test/specs/failure.spec.js
@@ -1,9 +1,5 @@
 const ParkingPage = require('../pageobjects/parking.page');
-
-
-const parkingLot = {VALETPARKING:1 , SHORTTERM:2 , ECONOMY:3 , LONGTEMGARAGE:4 , LONGTEMSURFAC:5};
-
-
+const valetParking = require('../utilities').VALETPARKING;
 
 describe('Basic elements interact', () => {
     beforeEach(() => {
@@ -11,7 +7,7 @@ describe('Basic elements interact', () => {
     })
     // DONE
     it('should throw an error if the start time is lower than leave time', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/9/2020');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -23,7 +19,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error if the start date is greater than leave date', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/10/2020');
         ParkingPage.inputStartingTime().setValue('11:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -35,7 +31,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error if the start date and time is greater than leave date and time', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/10/2020');
         ParkingPage.inputStartingTime().setValue('11:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -46,7 +42,7 @@ describe('Basic elements interact', () => {
         expect(ParkingPage.errorMessage()).toBeDisplayed();
     });
     it('should throw an error with a incorrect date in start date input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('14/32/2020');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -58,7 +54,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error with a incorrect date in leave date input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('14/32/2020');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -70,7 +66,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error with no valid[text, no format] input in start date input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('MM/DD/DDD');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -82,7 +78,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error with no valid[text,no format] input in leave date input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/10/2020');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -94,7 +90,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error with invalid time in start time input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/10/2020');
         ParkingPage.inputStartingTime().setValue('50:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -106,7 +102,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error with invalid time in leave time input', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/10/2020');
         ParkingPage.inputStartingTime().setValue('12:00');
         ParkingPage.radioButtonStartingAM().click();
@@ -118,7 +114,7 @@ describe('Basic elements interact', () => {
     });
 
     it('should throw an error if the start time is lower than leave time with AM/PM', () => {
-        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.comboBoxParkingLot().$(`//option[${valetParking}]`).click();
         ParkingPage.inputStartingDate().setValue('9/9/2020');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingPM().click();
