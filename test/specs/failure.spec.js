37fa5fdd238171cdb99d9ad2f874bd10a258bb53
yoshuadupin <djosue@unitec.edu>
2020-09-10 19:28:58 -0600

    added two more test for failure

--- test/specs/failure.spec.js
+++ test/specs/failure.spec.js
@@ -56,13 +56,37 @@ describe('Basic elements interact', () => {
         ParkingPage.buttonCalculate().click();
         expect(ParkingPage.errorMessage()).toBeDisplayed();
     });
-    it('should throw an error with no valid input in date', () => {
+    xit('should throw an error with no valid input in date', () => {
         ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
         ParkingPage.inputStartingDate().setValue('MM/DD/DDD');
         ParkingPage.inputStartingTime().setValue('10:00');
         ParkingPage.radioButtonStartingAM().click();
         ParkingPage.inputLeavingDate().setValue('MM/DD/DD');
         ParkingPage.inputLeavingTime().setValue('11:00');
+        ParkingPage.radioButtonLeavingAM().click();
+        ParkingPage.buttonCalculate().click();
+        expect(ParkingPage.errorMessage()).toBeDisplayed();
+    });
+
+    it('should throw an error with invalid time in start time input', () => {
+        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.inputStartingDate().setValue('9/10/2020');
+        ParkingPage.inputStartingTime().setValue('50:00');
+        ParkingPage.radioButtonStartingAM().click();
+        ParkingPage.inputLeavingDate().setValue('9/11/2020');
+        ParkingPage.inputLeavingTime().setValue('12:00');
+        ParkingPage.radioButtonLeavingAM().click();
+        ParkingPage.buttonCalculate().click();
+        expect(ParkingPage.errorMessage()).toBeDisplayed();
+    });
+
+    it('should throw an error with invalid time in leave time input', () => {
+        ParkingPage.comboBoxParkingLot().$(`//option[${parkingLot.VALETPARKING}]`).click();
+        ParkingPage.inputStartingDate().setValue('9/10/2020');
+        ParkingPage.inputStartingTime().setValue('12:00');
+        ParkingPage.radioButtonStartingAM().click();
+        ParkingPage.inputLeavingDate().setValue('9/11/2020');
+        ParkingPage.inputLeavingTime().setValue('50:00');
         ParkingPage.radioButtonLeavingAM().click();
         ParkingPage.buttonCalculate().click();
         expect(ParkingPage.errorMessage()).toBeDisplayed();
